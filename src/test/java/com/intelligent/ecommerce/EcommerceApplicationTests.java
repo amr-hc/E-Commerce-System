78e4cc4a6cd432597be356c57264a9fd1cd13703
amr-hc <amro7275@gmail.com>
2025-09-22 21:08:18 +0400

    Init Application

---
+++ src/test/java/com/intelligent/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelligent.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
